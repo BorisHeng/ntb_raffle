7d2ed4c175d9b3cd81611b543972320d599e1dee
Boris <s12113251@stu.edu.tw>
2020-05-13 02:54:00 +0800

    first commit

--- js/vue.js
+++ js/vue.js
@@ -1,3 +1,21 @@
+$(document).on("click", "a", function (event) {
+  let target = $(this).attr("href");
+  console.log(target);
+  if (
+    target == "#jumbotron" ||
+    target == "#sectiom_info" ||
+    target == "#sectiom_mv"
+  ) {
+    event.preventDefault(); //不執行預設動作
+    $("html, body").animate(
+      {
+        scrollTop: $(target).offset().top,
+      },
+      500
+    );
+  }
+});
+
 let app = new Vue({
   el: "#app",
   data: {
